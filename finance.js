5535b7bb07e82bbe4f363a04ab7a1614749aa379
Matt Fletcher <naiveamoeba@gmail.com>
2016-04-14 15:36:59 +0100

    First lot of CLI style stuff

--- finance.js
+++ finance.js
@@ -1,24 +1,71 @@
 #!/usr/bin/env node
+'use strict';
 
-var _ = require('underscore')._;
-var express = require('express');
+// Rows are: Date, Type, Description, Value, Balance, Account Name, Account Number
 
-var server = express();
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const inquirer = require('inquirer');
+const emoji = require('node-emoji');
 
-server.use(express.static(__dirname + '/frontend/public/'));
-server.listen(1234);
+const Server = require('./lib/server.js');
+const List = require('./lib/list.js');
 
-console.log('Natwest Finance Tool started on port 1234.');
+const port = 1234;
 
-var getData = require('./src/getData.js');
+GLOBAL.paths = {
+  CSV_PATH: './place-csvs-here',
+  CLIENT_PATH: './frontend/public',
+}
 
-server.get('/get', function (req, res) {
-  getData.get(function (data) {
-    res.send(data);
-  })
-});
+glob(`${GLOBAL.paths.CSV_PATH}/*.csv`, (err, files) => {
 
-server.get('/', function (req, res) {
-  res.sendFile('./index.html');
-  res.end()
+  if (err || !files.length) {
+    console.warn('You have not got any CSVs to parse through!');
+    inquirer.prompt([{
+      type: 'list',
+      name: 'action',
+      message: 'Please choose an action:',
+      choices: [
+        {value: 'setup', name: 'Setup your files with our handy wizard (doesn\'t work)'},
+        {value: 'exit', name: 'Exit'},
+      ]
+    }]).then((answers) => {
+      if (answers.action === 'setup') {
+        console.log('Yeah, sorry.');
+      }
+      if (answers.action === 'exit') {
+        process.exit(0);
+      }
+    });
+  }
+  else {
+    inquirer.prompt([{
+      type: 'list',
+      name: 'action',
+      message: 'Please choose an action:',
+      choices: [
+        {value: 'server', name: `Start up a server on port ${port}`},
+        {value: 'view_all', name: 'View all transactions'},
+        {value: 'view_incoming', name: `View INCOMING transactions ${emoji.get('sunglasses')}`},
+        {value: 'view_outgoing', name: `View OUTGOING transactions ${emoji.get('cold_sweat')}`}
+      ]
+    }]).then((answers) => {
+      if (answers.action === 'server') {
+        return new Server({port: port});
+      }
+      if (answers.action === 'view_all') {
+        return new List();
+      }
+      if (answers.action === 'view_incoming') {
+        return new List({filter: 'incoming'});
+      }
+      if (answers.action === 'view_outgoing') {
+        return new List({filter: 'outgoing'});
+      }
+    });
+  }
+
+
 });
