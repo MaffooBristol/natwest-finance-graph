a059b36d86e0b9a8e2ccd858bf2980f9bbb96474
Matt Fletcher <naiveamoeba@gmail.com>
2016-08-24 12:41:41 +0100

    Upgraded eslint-config-standard

--- client2/lib/Components/Transactions.jsx
+++ client2/lib/Components/Transactions.jsx
@@ -85,7 +85,7 @@ export default class TransactionList extends React.Component {
           />
         </Table>
       </div>
-    );*/
+    ); */
     return (
       <FlexTable
         ref='Table'
